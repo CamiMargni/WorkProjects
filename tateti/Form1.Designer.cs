1a452c071242b0955719de5e0ab3d2aa9aa73991
Camila Margni <camiimargni@gmail.com>
2020-06-16 11:09:11 -0300

    Add the complete project to github

---
+++ tateti/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace tateti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sup1 = new System.Windows.Forms.Button();
+            this.sup2 = new System.Windows.Forms.Button();
+            this.sup3 = new System.Windows.Forms.Button();
+            this.medio1 = new System.Windows.Forms.Button();
+            this.medio2 = new System.Windows.Forms.Button();
+            this.medio3 = new System.Windows.Forms.Button();
+            this.inf1 = new System.Windows.Forms.Button();
+            this.inf2 = new System.Windows.Forms.Button();
+            this.inf3 = new System.Windows.Forms.Button();
+            this.titulo = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.juegaUsuario = new System.Windows.Forms.Button();
+            this.juegaPc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sup1
+            // 
+            this.sup1.AccessibleName = "sup1";
+            this.sup1.Location = new System.Drawing.Point(270, 125);
+            this.sup1.Name = "sup1";
+            this.sup1.Size = new System.Drawing.Size(75, 23);
+            this.sup1.TabIndex = 0;
+            this.sup1.UseVisualStyleBackColor = true;
+            this.sup1.Click += new System.EventHandler(this.Sup1_Click);
+            // 
+            // sup2
+            // 
+            this.sup2.AccessibleName = "sup2";
+            this.sup2.Location = new System.Drawing.Point(351, 125);
+            this.sup2.Name = "sup2";
+            this.sup2.Size = new System.Drawing.Size(75, 23);
+            this.sup2.TabIndex = 1;
+            this.sup2.UseVisualStyleBackColor = true;
+            this.sup2.Click += new System.EventHandler(this.Sup2_Click);
+            // 
+            // sup3
+            // 
+            this.sup3.AccessibleName = "sup3";
+            this.sup3.Location = new System.Drawing.Point(432, 125);
+            this.sup3.Name = "sup3";
+            this.sup3.Size = new System.Drawing.Size(75, 23);
+            this.sup3.TabIndex = 2;
+            this.sup3.UseVisualStyleBackColor = true;
+            this.sup3.Click += new System.EventHandler(this.Sup3_Click);
+            // 
+            // medio1
+            // 
+            this.medio1.AccessibleName = "medio1";
+            this.medio1.Location = new System.Drawing.Point(270, 154);
+            this.medio1.Name = "medio1";
+            this.medio1.Size = new System.Drawing.Size(75, 23);
+            this.medio1.TabIndex = 3;
+            this.medio1.UseVisualStyleBackColor = true;
+            this.medio1.Click += new System.EventHandler(this.Medio1_Click);
+            // 
+            // medio2
+            // 
+            this.medio2.AccessibleName = "medio2";
+            this.medio2.Location = new System.Drawing.Point(351, 154);
+            this.medio2.Name = "medio2";
+            this.medio2.Size = new System.Drawing.Size(75, 23);
+            this.medio2.TabIndex = 4;
+            this.medio2.UseVisualStyleBackColor = true;
+            this.medio2.Click += new System.EventHandler(this.Medio2_Click);
+            // 
+            // medio3
+            // 
+            this.medio3.AccessibleName = "medio3";
+            this.medio3.Location = new System.Drawing.Point(432, 154);
+            this.medio3.Name = "medio3";
+            this.medio3.Size = new System.Drawing.Size(75, 23);
+            this.medio3.TabIndex = 5;
+            this.medio3.UseVisualStyleBackColor = true;
+            this.medio3.Click += new System.EventHandler(this.Medio3_Click);
+            // 
+            // inf1
+            // 
+            this.inf1.AccessibleName = "inf1";
+            this.inf1.Location = new System.Drawing.Point(270, 184);
+            this.inf1.Name = "inf1";
+            this.inf1.Size = new System.Drawing.Size(75, 23);
+            this.inf1.TabIndex = 6;
+            this.inf1.UseVisualStyleBackColor = true;
+            this.inf1.Click += new System.EventHandler(this.Inf1_Click);
+            // 
+            // inf2
+            // 
+            this.inf2.AccessibleName = "inf2";
+            this.inf2.Location = new System.Drawing.Point(351, 183);
+            this.inf2.Name = "inf2";
+            this.inf2.Size = new System.Drawing.Size(75, 23);
+            this.inf2.TabIndex = 7;
+            this.inf2.UseVisualStyleBackColor = true;
+            this.inf2.Click += new System.EventHandler(this.Inf2_Click);
+            // 
+            // inf3
+            // 
+            this.inf3.AccessibleName = "inf3";
+            this.inf3.Location = new System.Drawing.Point(433, 183);
+            this.inf3.Name = "inf3";
+            this.inf3.Size = new System.Drawing.Size(75, 23);
+            this.inf3.TabIndex = 8;
+            this.inf3.UseVisualStyleBackColor = true;
+            this.inf3.Click += new System.EventHandler(this.Inf3_Click);
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.titulo.Location = new System.Drawing.Point(364, 94);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(51, 13);
+            this.titulo.TabIndex = 9;
+            this.titulo.Text = "TA TE TI";
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(348, 225);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(81, 13);
+            this.resultado.TabIndex = 10;
+            this.resultado.Text = "Juego en Curso";
+            this.resultado.Click += new System.EventHandler(this.Resultado_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(567, 138);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(70, 55);
+            this.reset.TabIndex = 11;
+            this.reset.TabStop = false;
+            this.reset.Text = "Jugar otra vez!";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // juegaUsuario
+            // 
+            this.juegaUsuario.Location = new System.Drawing.Point(308, 281);
+            this.juegaUsuario.Name = "juegaUsuario";
+            this.juegaUsuario.Size = new System.Drawing.Size(75, 23);
+            this.juegaUsuario.TabIndex = 12;
+            this.juegaUsuario.Text = "juego yo";
+            this.juegaUsuario.UseVisualStyleBackColor = true;
+            this.juegaUsuario.Click += new System.EventHandler(this.JuegaUsuario_Click);
+            // 
+            // juegaPc
+            // 
+            this.juegaPc.Location = new System.Drawing.Point(389, 281);
+            this.juegaPc.Name = "juegaPc";
+            this.juegaPc.Size = new System.Drawing.Size(75, 23);
+            this.juegaPc.TabIndex = 13;
+            this.juegaPc.Text = "juega la pc";
+            this.juegaPc.UseVisualStyleBackColor = true;
+            this.juegaPc.Click += new System.EventHandler(this.JuegaPc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.juegaPc);
+            this.Controls.Add(this.juegaUsuario);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.inf3);
+            this.Controls.Add(this.inf2);
+            this.Controls.Add(this.inf1);
+            this.Controls.Add(this.medio3);
+            this.Controls.Add(this.medio2);
+            this.Controls.Add(this.medio1);
+            this.Controls.Add(this.sup3);
+            this.Controls.Add(this.sup2);
+            this.Controls.Add(this.sup1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sup1;
+        private System.Windows.Forms.Button sup2;
+        private System.Windows.Forms.Button sup3;
+        private System.Windows.Forms.Button medio1;
+        private System.Windows.Forms.Button medio2;
+        private System.Windows.Forms.Button medio3;
+        private System.Windows.Forms.Button inf1;
+        private System.Windows.Forms.Button inf2;
+        private System.Windows.Forms.Button inf3;
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button juegaUsuario;
+        private System.Windows.Forms.Button juegaPc;
+    }
+}
+
---
+++ tateti/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tateti
+{
+    public partial class Form1 : Form
+    {
+        bool jugadaGanadora(int idBoton, int[] tablero)
+        {
+
+            switch (idBoton)
+            {
+                case 0:
+                    return (((tablero[1] == tablero[idBoton]) && (tablero[1] == tablero[2])) || ((tablero[3] == tablero[6]) && (tablero[6] == tablero[idBoton])) || ((tablero[4] == tablero[8]) && (tablero[4] == tablero[idBoton])));
+                  
+                case 1:
+                    return (((tablero[0] == tablero[idBoton]) && (tablero[0] == tablero[2])) || ((tablero[4] == tablero[idBoton]) && (tablero[4] == tablero[7])));
+                    
+                case 2:
+                    return (((tablero[1] == tablero[idBoton]) && (tablero[1] == tablero[0])) || ((tablero[5] == tablero[idBoton]) && (tablero[5] == tablero[8])) || ((tablero[4] == tablero[idBoton]) && (tablero[4] == tablero[6])));
+                    
+                case 3:
+                    return (((tablero[0] == tablero[idBoton]) && (tablero[0] == tablero[6])) || ((tablero[4] == tablero[idBoton]) && (tablero[5] == tablero[4])));
+                    
+                case 4:
+                    return (((tablero[0] == tablero[idBoton]) && (tablero[0] == tablero[8])) || ((tablero[2] == tablero[idBoton]) && (tablero[2] == tablero[6])) || ((tablero[1] == tablero[idBoton]) && (tablero[1] == tablero[7])) || ((tablero[3] == tablero[idBoton]) && (tablero[3] == tablero[5])));
+                    
+                case 5:
+                    return (((tablero[2] == tablero[idBoton]) && (tablero[8] == tablero[2])) || ((tablero[4] == tablero[idBoton]) && (tablero[4] == tablero[3])));
+                    
+                case 6:
+                    return (((tablero[0] == tablero[idBoton]) && (tablero[0] == tablero[3])) || ((tablero[7] == tablero[idBoton]) && (tablero[7] == tablero[8])));
+                    
+                case 7:
+                    return (((tablero[1] == tablero[idBoton]) && (tablero[1] == tablero[4])) || ((tablero[6] == tablero[idBoton]) && (tablero[6] == tablero[8])));
+                    
+                case 8:
+                    return (((tablero[2] == tablero[idBoton]) && (tablero[5] == tablero[2])) || ((tablero[6] == tablero[idBoton]) && (tablero[6] == tablero[7])));
+                    
+                default: return false;
+            }
+        }
+
+        void jugadorGanador(int idBoton, int[] tablero)
+        {
+            if (jugadaGanadora(idBoton, tablero))
+            {
+                if(tablero[idBoton] == 1)
+                {
+                MessageBox.Show("TE GANÓ LA MÁQUINA, SORRY");
+                }
+                else if(tablero[idBoton] == 2)
+                {
+                    MessageBox.Show("GANASTE!");
+                }
+            }
+        }
+
+        void jugadaDeMaquina(Button botonGenerico, int[] unTablero, bool flag)
+        {
+            if (flag)
+            {
+                botonGenerico.BackColor = Color.Pink;
+                botonGenerico.Text = "x";
+                incrementarTurno();
+            }
+
+            unTablero[botonGenerico.TabIndex] = 1;
+            
+        }
+
+        void jugadaDePersona(Button botonGenerico, int[] unTablero, bool flag)
+        {
+            if (flag) {
+                botonGenerico.BackColor = Color.Blue;
+                botonGenerico.Text = "O";
+                incrementarTurno();
+            }
+            
+            unTablero[botonGenerico.TabIndex] = 2;
+            
+        }
+
+        Button evaluarJugada(Button botonGenerico, int[] tablero) //devuelve el numero de posición que debería ocuparse por la máquina
+        {
+            Button[] botones = new Button[] { sup1, sup2, sup3, medio1, medio2, medio3, inf1, inf2, inf3 };
+
+            for (int u = 0; u < 8; u++)
+            {
+                //primero me fijo si la máquina puede ganar con la próxima jugada
+                int[] copia = duplicadoDeTablero(tablero);
+                if (estaLibre(u, copia))
+                {
+                    jugadaDeMaquina(botones[u],copia, false);
+                    if (jugadaGanadora(u, copia))
+                    {
+                        return botones[u]; //retorno la posición libre con la que puedo ganar
+                    }
+                }
+            }
+
+            //me fijo si la persona puede ganar para bloquearla            
+            for (int h = 0; h < 8; h++)
+            {
+                int[] copia = duplicadoDeTablero(tablero);
+                if (estaLibre(h, copia))
+                {
+                    jugadaDePersona(botones[h], copia, false);
+                    if (jugadaGanadora(h, copia))
+                    {
+                        return botones[h]; //retorna la posición que hay que bloquear
+                    }
+                }
+            }
+
+            //coloca en cualquier esquina
+            int jugada = elegirAlAzarDe(tablero, esquinas);
+            if (jugada != 1000)
+            {
+                return botones[jugada];
+            }
+
+            //coloca en el medio
+            if (estaLibre(4, tablero))
+            {
+                return botones[4];
+            }
+            
+            //coloca en algun lado
+            int posicion = elegirAlAzarDe(tablero, medio);
+            return botones[posicion];
+        }
+
+
+        Button primeraJugadaDeMaquina() //si el primer turno es de la máquina pone al azar
+        {
+            Button[] botones = new Button[] { sup1, sup2, sup3, medio1, medio2, medio3, inf1, inf2, inf3 };
+            int primerJugada = elegirAlAzarDe(tablero, posicionesTablero);
+            return botones[primerJugada];
+        }
+        Boolean estaLibre(int posicion, int[] unTablero) //dice si la celda todavía no fue usada
+        {
+            return unTablero[posicion] == 0; 
+        }
+
+        int elegirAlAzarDe(int[] tablero, int[] posiciones)
+        {
+            Random rand = new Random();
+            
+
+            int[] jugadasPosibles = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1};
+            int i = 0;
+                  
+
+            for (int j = 0; j < posiciones.Length; j++)
+            {
+                if (estaLibre(posiciones[j], tablero))
+                {
+                    jugadasPosibles[i] = posiciones[j];
+                    i++;
+                }
+            }
+            int y = 0;
+            while (jugadasPosibles[y] != -1 && y<8){
+                y++;
+            }
+           
+            if (y > 0)
+            {
+                int index = rand.Next(y-1);
+                return jugadasPosibles[index];
+            }
+            else
+            {
+                return 1000; //retorna error - que no hay jugadas posibles 
+            }
+        }
+
+        int[] duplicadoDeTablero(int[] tablero)
+        {
+            int[] copia = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < tablero.Length; i++)
+            {
+                copia[i] = tablero[i];
+            }
+            return copia;
+        }
+
+        void incrementarTurno(){
+            turno++;
+        }
+
+        void empate(int unTurno)
+        {
+            if (unTurno == 9)
+            {
+                MessageBox.Show("Empate");
+            }
+        }
+
+        void respuestaDeLaMaquina(Button boton, int unTurno)
+        {
+            proximaJugada = evaluarJugada(boton, tablero);
+            jugadaDeMaquina(proximaJugada, tablero, true);
+            jugadorGanador(proximaJugada.TabIndex, tablero);
+            empate(unTurno);
+        }
+        
+        int[] tablero = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int turno = 0;
+        int[] esquinas = new int[] { 0, 2, 6, 8 };
+        int[] posicionesTablero = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[] medio = new int[] { 1, 3, 5, 7 };
+        Button proximaJugada;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sup1_Click(object sender, EventArgs e) //este es el primero que pone la máquina siempre
+        {
+            jugadaDePersona(sup1, tablero, true);
+            jugadorGanador(sup1.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(sup1.TabIndex, tablero) && turno <9)
+            {
+                respuestaDeLaMaquina(sup1, turno);
+            }
+        }
+
+        private void Sup2_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(sup2, tablero, true);
+            jugadorGanador(sup2.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(sup2.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(sup2, turno);
+            }
+        }
+
+        private void Sup3_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(sup3, tablero, true);
+            jugadorGanador(sup3.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(sup3.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(sup3, turno);
+            }
+        }
+
+        private void Medio1_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(medio1, tablero, true);
+            jugadorGanador(medio1.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(medio1.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(medio1, turno);
+            }
+        }
+
+        private void Medio2_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(medio2, tablero, true);
+            jugadorGanador(medio2.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(medio2.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(medio2, turno);
+            }
+        }
+
+        private void Medio3_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(medio3, tablero, true);
+            jugadorGanador(medio3.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(medio3.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(medio3, turno);
+            }
+        }
+
+        private void Inf1_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(inf1, tablero, true);
+            jugadorGanador(inf1.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(inf1.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(inf1, turno);
+            }
+        }
+
+        private void Inf2_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(inf2, tablero, true);
+            jugadorGanador(inf2.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(inf2.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(inf2, turno);
+            }
+        }
+
+        private void Inf3_Click(object sender, EventArgs e)
+        {
+            jugadaDePersona(inf3, tablero, true);
+            jugadorGanador(inf3.TabIndex, tablero);
+
+            empate(turno);
+
+            if (!jugadaGanadora(inf3.TabIndex, tablero) && turno < 9)
+            {
+                respuestaDeLaMaquina(inf3, turno);
+            }
+        }
+
+        private void Resultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reset.BackColor = Color.Aquamarine;
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void JuegaUsuario_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void JuegaPc_Click(object sender, EventArgs e)
+        {
+            Button primerJugada = primeraJugadaDeMaquina();
+            jugadaDeMaquina(primerJugada, tablero, true);
+            
+        }
+    }
+    
+}
+ 
+ 
+ 
